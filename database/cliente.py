807a2ca8c65fa82de4a3e9247283e7548113a703
TOKENT <dgalvao2413@gmail.com>
2024-11-05 10:55:57 -0300

    Second Day - CRUD Complete

---
+++ database/cliente.py
@@ -0,0 +1,9 @@
+CLIENTES = [
+  {"id": 1, "nome": "Daniel", "email": "teste@teste.com"},
+  {"id": 2, "nome": "Jorge", "email": "aaaaa@teste.com"},
+  {"id": 3, "nome": "Felipe", "email": "bbbbb@teste.com"},
+  {"id": 4, "nome": "Joao", "email": "ccccc@teste.com"},
+  {"id": 5, "nome": "Otavio", "email": "eeeeee@teste.com"},
+  {"id": 6, "nome": "Nicoly", "email": "fffffff@teste.com"},
+  {"id": 7, "nome": "Helena", "email": "gggggggg@teste.com"},
+]
